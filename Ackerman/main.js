adb619df71ce28968f48a0c3d2437b7f16f68cc2
OrangeInky <86501049+OrangeInky@users.noreply.github.com>
2021-11-09 20:56:17 -0500

    Add files via upload

---
+++ Ackerman/main.js
@@ -0,0 +1,7 @@
+function Begin() {
+    state = true;
+    window.setInterval( function() {
+        document.getElementById('B').style.display = "none"
+        document.getElementById("Display").append("YO MAMA FAT LOL \n XD XD XD LMAO LMAO \n");
+    },1)
+}
