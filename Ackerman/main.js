c3ec9fb3c5fae724a185605ff043c82cf4512fcf
OrangeInky <86501049+OrangeInky@users.noreply.github.com>
2021-11-09 20:56:58 -0500

    Delete Ackerman directory

--- Ackerman/main.js
+++
@@ -1,7 +0,0 @@
-function Begin() {
-    state = true;
-    window.setInterval( function() {
-        document.getElementById('B').style.display = "none"
-        document.getElementById("Display").append("YO MAMA FAT LOL \n XD XD XD LMAO LMAO \n");
-    },1)
-}
